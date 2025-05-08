8a7c6bc8b3c138c7ed8c8c465e9397b0778762bc
bineeshb <bbineesh21@gmail.com>
2025-05-08 08:12:41 +0530

    Add nav

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { RouterOutlet, provideRouter, withComponentInputBinding } from '@angular/router';
+import { Router, RouterOutlet, provideRouter, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 @Component({
   selector: 'app-root',
   template: `
     <h1>NG Playground</h1>
+    <select (change)="goto($event)">
+    @for(route of routes; track route.path) {
+      <option [value]="route.path">{{ route.path }}</option>
+    }
+    </select>
     <router-outlet></router-outlet>
   `,
   styles: `
@@ -17,7 +22,17 @@ import { appRoutes } from './app.routes';
   `,
   imports: [RouterOutlet],
 })
-export class App { }
+export class App {
+  routes = appRoutes.filter(({ path }) => !!path);
+  router = inject(Router);
+
+  goto(event: Event): void {
+    const route = (event.target as HTMLInputElement)?.value;
+    if (typeof route === 'string' && route.length > 0) {
+      this.router.navigate([route]);
+    }
+  }
+}
 
 bootstrapApplication(App, {
   providers: [provideRouter(appRoutes, withComponentInputBinding())]
